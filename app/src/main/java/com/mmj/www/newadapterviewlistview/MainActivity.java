46c5ca687e142c244b49585fb5ae4bb9e20e4dad
kojomba69 <kojomba@naver.com>
2018-11-06 12:21:31 +0900

    2st

--- app/src/main/java/com/mmj/www/newadapterviewlistview/MainActivity.java
+++ app/src/main/java/com/mmj/www/newadapterviewlistview/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //comment 2
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String[] FRUITS = new String[] {"APPLE", "PEAR", "GRAPE", "PERSIMMON"};
